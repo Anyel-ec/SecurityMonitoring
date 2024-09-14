f5f5877dc5b4b34b8c315fb4dc90c29801c9ba83
anyel-ec <paulangel826@gmail.com>
2024-09-14 14:09:44 -0500

    chore: start project in spring boot 3.3.3 JDK 17, configurate dependencies

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/BackendApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.security.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/ec/edu/espe/security/monitoring/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.security.monitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
