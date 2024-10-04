cd744528e35ce9192f82e2a6af89bc56c0d838ce
anyel-ec <paulangel826@gmail.com>
2024-10-03 19:13:33 -0500

    chore: add repositories to install

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/repositories/PrometheusInstallRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.repositories;
+
+import ec.edu.espe.security.monitoring.models.installed.PrometheusInstall;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PrometheusInstallRepository extends JpaRepository<PrometheusInstall, Long> {
+}
