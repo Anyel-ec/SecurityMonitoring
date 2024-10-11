0538f788a32a674476171a25201da0c35ad7f8cd
anyel-ec <paulangel826@gmail.com>
2024-10-11 10:33:15 -0500

    feat: consume service in frotend to run DockerCompose

--- Backend/src/main/java/ec/edu/espe/security/monitoring/services/implementations/docker/DockerComposeServiceImpl.java
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/services/implementations/docker/DockerComposeServiceImpl.java
@@ -1,6 +1,7 @@
 package ec.edu.espe.security.monitoring.services.implementations;
 
 import ec.edu.espe.security.monitoring.models.credentials.DatabaseCredentials;
+import ec.edu.espe.security.monitoring.services.interfaces.docker.DockerComposeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -8,11 +9,38 @@ import java.io.IOException;
 
 @Service
 @Slf4j
-public class DockerServiceImpl {
+public class DockerComposeServiceImpl implements DockerComposeService {
+    /**
+     * Runs Docker Compose to bring up the services defined in the docker-compose.yml file.
+     * This method simply executes the docker-compose up -d command.
+     */
+    @Override
+    public void runDockerCompose() {
+        ProcessBuilder processBuilder = new ProcessBuilder(
+                "docker-compose",
+                "-f", "../.container/docker-compose.yml",
+                "up", "-d"
+        );
+
+        try {
+            // Log before execution
+            log.info("Executing docker-compose up -d to bring up services...");
+
+            // Inherit IO to display output in the current terminal and start the process
+            processBuilder.inheritIO().start().waitFor();
+
+            log.info("Docker Compose executed successfully.");
+        } catch (IOException | InterruptedException e) {
+            log.error("Error while executing docker-compose up: ", e);
+            Thread.currentThread().interrupt();  // Restore the interrupted status if interrupted
+        }
+    }
+
     /**
      * Runs a Docker Compose process to set up a database container based on the provided DBMS type and credentials.
      */
-    public void runDockerCompose(DatabaseCredentials config, String dbType) throws IOException {
+    @Override
+    public void runDockerComposeWithDatabase(DatabaseCredentials config, String dbType) throws IOException {
         ProcessBuilder processBuilder = new ProcessBuilder(
                 "docker-compose",
                 "-f", "../.container/docker-compose.yml",
---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/services/interfaces/docker/DockerComposeService.java
@@ -0,0 +1,5 @@
+package ec.edu.espe.security.monitoring.services.interfaces.docker;
+
+public interface DockerService {
+    void runDockerCompose();
+}
