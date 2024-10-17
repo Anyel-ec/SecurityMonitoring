12439607f13b60780c9a4414efdfcdc1aa54eadf
anyel-ec <paulangel826@gmail.com>
2024-10-16 21:52:04 -0500

    feat: encrypt/decrypt RSA with padding OAEP

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/controllers/AuthController.java
@@ -0,0 +1,8 @@
+package ec.edu.espe.security.monitoring.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthController {
+
+}
---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/services/implementations/encrypt/RsaServiceImpl.java
@@ -0,0 +1,33 @@
+package ec.edu.espe.security.monitoring.services.implementations.encrypt;
+
+import ec.edu.espe.security.monitoring.utils.RsaEncryptUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class RsaServiceImpl {
+
+    @Value("${rsa.private.key}")
+    private String environmentPrivateKey;
+    @Autowired
+    private RsaEncryptUtil rsaEncryptUtil;
+
+    public String loginCliente(String data) throws IllegalBlockSizeException, NoSuchPaddingException, BadPaddingException, NoSuchAlgorithmException, InvalidKeyException {
+
+        // Desencriptar los datos recibidos
+        String decryptedPayload = rsaEncryptUtil.decrypt(data, environmentPrivateKey);
+
+        return "in development....";
+    }
+}
