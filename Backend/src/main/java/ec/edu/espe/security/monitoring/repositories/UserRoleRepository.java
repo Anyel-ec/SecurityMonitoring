ca0806a554267862677fb933f4fab5e87dc276ae
anyel-ec <paulangel826@gmail.com>
2024-11-04 12:02:55 -0500

    chore: create repository to role the user

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/repositories/UserRoleRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.repositories;
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 04/11/2024
+ */
+ 
+public class UserRoleRepository {
+}
