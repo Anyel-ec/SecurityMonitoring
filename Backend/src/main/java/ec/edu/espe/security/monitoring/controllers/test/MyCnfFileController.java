4b9b46739273129f8e41fdab7748c7465ba0f908
anyel-ec <paulangel826@gmail.com>
2024-11-16 19:01:47 -0500

    chore: add MyCnfFileController class to the monitoring controllers package

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/controllers/test/MyCnfFileController.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.controllers.test;
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 16/11/2024
+ */
+ 
+public class MyCnfFileController {
+}
