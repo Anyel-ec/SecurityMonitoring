2a62c60ada18277ff0ecb668b380392f424ef5c6
anyel-ec <paulangel826@gmail.com>
2024-11-25 23:11:27 -0500

    feat: add AlertManagerConfigUtil utility class for Alertmanager configuration

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/utils/AlertManagerConfigUtil.java
@@ -0,0 +1,14 @@
+package ec.edu.espe.security.monitoring.utils;
+
+import lombok.experimental.UtilityClass;
+import lombok.extern.slf4j.Slf4j;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 25/11/2024
+ */
+@UtilityClass
+@Slf4j
+public class AlermanagerConfigUtil {
+}
