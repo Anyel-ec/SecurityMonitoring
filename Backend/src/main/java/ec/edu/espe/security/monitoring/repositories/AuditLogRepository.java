5498c5d4b0b2233e03f7470239a4db343d5a274c
anyel-ec <paulangel826@gmail.com>
2024-12-25 15:15:58 -0500

    chore: add audit log

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/repositories/AuditLogRepository.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.repositories;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 25/12/2024
+ */
+public class AuditLog {
+}
---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/services/impl/log/AuditLogServiceImpl.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.services.impl.log;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 25/12/2024
+ */
+public class AuditLogServiceImpl {
+}
