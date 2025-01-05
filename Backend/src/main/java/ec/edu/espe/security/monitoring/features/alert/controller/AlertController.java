f1905665e2985766550b139212783492ff3768f8
anyel-ec <paulangel826@gmail.com>
2025-01-04 23:32:56 -0500

    fix: correcte time expiration jwt

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/features/alert/controller/AlertController.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.features.alert.controller;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 04/01/2025
+ */
+public class AlertController {
+}
---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/features/alert/services/AlertService.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.features.alert.services;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 04/01/2025
+ */
+public class AlertService {
+}
