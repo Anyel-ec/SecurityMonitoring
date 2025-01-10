ddff8af3fce926c43e78fb647e5527c98099e27d
anyel-ec <paulangel826@gmail.com>
2025-01-10 08:06:42 -0500

    feat: add Profile management classes including controller, DTOs, and service implementations

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/modules/features/auth/dto/ProfileDto.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.modules.features.auth.dto;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 10/01/2025
+ */
+public class ProfileDto {
+}
