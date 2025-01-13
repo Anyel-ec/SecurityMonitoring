54f95e8ededeaee835aee617b30bfc84ac07bc72
anyel-ec <paulangel826@gmail.com>
2025-01-13 03:55:35 -0500

    feat: add PasswordRecovery model and repository classes

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/modules/features/auth/model/PasswordRecovery.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.modules.features.auth.model;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 13/01/2025
+ */
+public class PasswordRecovery {
+}
