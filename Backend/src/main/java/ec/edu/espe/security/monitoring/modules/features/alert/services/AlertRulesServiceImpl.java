8cc6552a6bd555714a4ef341d68ddbe85ab4282f
anyel-ec <paulangel826@gmail.com>
2025-01-14 18:44:29 -0500

    feat: add AlertRulesController, AlertRulesService, and AlertRulesServiceImpl classes

---
+++ Backend/src/main/java/ec/edu/espe/security/monitoring/modules/features/alert/services/AlertRulesServiceImpl.java
@@ -0,0 +1,9 @@
+package ec.edu.espe.security.monitoring.modules.features.alert.services;
+
+/*
+ * Author: Anyel EC
+ * Github: https://github.com/Anyel-ec
+ * Creation date: 14/01/2025
+ */
+public class AlertRulesServiceImpl {
+}
